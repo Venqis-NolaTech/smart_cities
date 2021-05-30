971967239e4d0d9ad006f5a6c6e056488a37e225
Eukaris Arreaza <eukaris@mail.com>
2021-05-30 18:03:01 -0400

    test plugin home_widget

--- android/app/src/main/kotlin/com/venqis/smartcities/HomeWidgetProvider.kt
+++ android/app/src/main/kotlin/com/venqis/smartcities/HomeWidgetProvider.kt
@@ -3,13 +3,49 @@ package com.venqis.smartcities
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.widget.RemoteViews
 import android.content.SharedPreferences
 
 abstract class HomeWidgetProvider : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
-        onUpdate(context, appWidgetManager, appWidgetIds, HomeWidgetPlugin.getData(context))
+        appWidgetIds.forEach { widgetId ->
+            val views = RemoteViews(context.packageName, R.layout.simple_app_widget).apply {
+                val pendingIntent = HomeWidgetLaunchIntent.getActivity(
+                    context,
+                    MainActivity::class.java)
+                setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+
+                /*
+                // Open App on Widget Click
+                val pendingIntent = HomeWidgetLaunchIntent.getActivity(
+                    context,
+                    MainActivity::class.java)
+                setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+
+                // Swap Title Text by calling Dart Code in the Background
+                setTextViewText(R.id.widget_title, widgetData.getString("title", null)
+                    ?: "No Title Set")
+                val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
+                    context,
+                    Uri.parse("homeWidgetExample://titleClicked")
+                )
+                setOnClickPendingIntent(R.id.widget_title, backgroundIntent)
+
+                val message = widgetData.getString("message", null)
+                setTextViewText(R.id.widget_message, message
+                    ?: "No Message Set")
+                // Detect App opened via Click inside Flutter
+                val pendingIntentWithData = HomeWidgetLaunchIntent.getActivity(
+                    context,
+                    MainActivity::class.java,
+                    Uri.parse("homeWidgetExample://message?message=$message"))
+                setOnClickPendingIntent(R.id.widget_message, pendingIntentWithData)*/
+            }
+
+            appWidgetManager.updateAppWidget(widgetId, views)
+        }
     }
 
     abstract fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences)
